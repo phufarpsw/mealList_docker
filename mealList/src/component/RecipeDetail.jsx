ac41d937278c25971de24a2492c02d0da64ef15f
phufarpsw <phufa_rpsw@hotmail.com>
2023-04-17 01:27:35 +0700

    edit recipeDetail

--- mealList/src/component/RecipeDetail.jsx
+++ mealList/src/component/RecipeDetail.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import kcal from "../assets/icon/icons8-kcal-58.png";
 import level from "../assets/icon/level.png";
-import time from "../assets/icon/clock.png";
+import time from "../assets/icon/Clock.png";
 import cook from "../assets/icon/cooking.png";
 import "../css/style.css";
 export default function RecipeDetail(props) {
@@ -35,22 +35,22 @@ export default function RecipeDetail(props) {
           />
           <div className="w-[90%] space-y-4">
             <div className="title-recipe-food mt-10">
-              <p className="font-kanit capitalize text-5xl">{recipe.name2}</p>
+              <p className="font-kanit capitalize text-5xl font-leckerli">{recipe.name2}</p>
               <p className="font-kanit capitalize text-xl pt-2">{recipe.name1}</p>
             </div>
             <div className="flex space-x-4">
               <div className=" flex justify-center items-center space-x-4 p-4 rounded-xl">
                 <img src={kcal} className="w-14" alt="" />
                 <div className="flex flex-col items-center">
-                  <p className="text-lg">{recipe.Kcal} Calories</p>
-                  <p className="text-sm text-gray-200">Energy</p>
+                  <p className="text-xl">Energy</p>
+                  <p className=" text-gray-200">{recipe.Kcal} Calories</p>
                 </div>
               </div>
               <div className="w-48 flex justify-center items-center space-x-4 p-4 rounded-xl">
                 <img src={level} className="h-12" alt="" />
                 <div className="flex flex-col items-center">
-                  <p className="text-2xl">Level</p>
-                  <p className="capitalize">{recipe.level}</p>
+                  <p className="text-xl capitalize">Difficulty</p>
+                  <p className="capitalize">{recipe.level} - Level</p>
                 </div>
               </div>
               <div className="flex justify-center items-center space-x-4 p-4 rounded-xl">
@@ -68,10 +68,10 @@ export default function RecipeDetail(props) {
                 </div>
               </div>
             </div>
-            <div id="ingredient" className="space-y-4">
+            <div id="ingredient" className="w-full space-y-4">
               <p className="text-3xl font-kanit text-rose-500 mt-5">Ingredients</p>
-              <div className="">
-                <ul className="list-disc pl-7">
+              <div className="w-full ">
+                <ul className="list-disc grid grid-cols-3 gap-y-2 pl-7">
                   {ingredient &&
                     ingredient.map((res, index) => {
                       return <li key={index} className="mt-1">{res}</li>;
@@ -79,15 +79,23 @@ export default function RecipeDetail(props) {
                 </ul>
               </div>
             </div>
-            <div className="method space-y-4">
+            <div className="method space-y-4 w-7/12">
               <p className="text-3xl font-kanit text-rose-500 mt-5">Method</p>
               <div className="">
                 {method &&
                   method.map((res, index) => {
-                    return <p key={index} className="mt-1">{index+1}. {res}</p>;
+                    return <p key={index} className="mt-2">{index+1}. {res}</p>;
                   })}
               </div>
             </div>
+            <section className="comment">
+              <p className="text-3xl text-rose-500">Review</p>
+              <p className=" text-gray-200">( 2 comments )</p>
+              <div className="w-full h-24 rounded-lg p-4 border flex flex-col justify-center mt-5">
+                <p id="name" className="text-rose-500 text-xl">Tiwat.rap</p>
+                <p>Lovely food. 100% recommed<br />The customer service was great :D </p>
+              </div>
+            </section>
           </div>
         </div>
       )}
